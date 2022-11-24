d6566cf216361b7d7eb0bc49061ff4c282a41e62
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-11-24 14:51:58 +0530

    add students

---
+++ src/app/student-entry/student-entry.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-entry',
+  templateUrl: './student-entry.component.html',
+  styleUrls: ['./student-entry.component.css']
+})
+export class StudentEntryComponent {
+
+}
